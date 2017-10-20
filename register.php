e8f3633e4707091c1b54edca439caf03376919f5
Vincent Truong <vtruong74@outlook.com>
2017-10-20 03:16:03 -0600

    Added user registration, updates database with new registration (username and password).

--- register.php
+++ register.php
@@ -13,3 +13,40 @@
     </form>
     </body>
 </html>
+
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = ($_POST['username']);
+        $password = ($_POST['password']);
+
+        echo "Username entered is: " . $username . "<br/>";
+        echo "Password entered is: " . $password;
+
+        $bool = true;
+
+        $conn = mysqli_connect("localhost","root") or die(mysqli_error()); //connect to server
+        $link = mysqli_select_db($conn,"php-login") or die("Cannot connect to database"); //connect to database
+        $query = mysqli_query($conn,"SELECT * FROM users");//query user tables
+
+        //if username already taken
+        while($row = mysqli_fetch_array($query))
+        {
+            $table_users=$row['username'];
+            if($username==$table_users)
+            {
+                $bool = false;
+                Print '<script>alert("Username taken!");</script>';
+                Print '<script>window.location.assign("register.php");</script>';
+            }
+        }
+
+        //successfully register user
+        if($bool)
+        {
+            mysqli_query($conn,"INSERT INTO users (username, password) VALUES ('$username', '$password')");
+            Print '<script>alert("Successfully Registered");</script>';
+            Print '<script>window.location.assign("register.php");</script>';
+        }
+
+    }
+?>
